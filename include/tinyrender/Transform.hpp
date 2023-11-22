cb428d4866fbb92e5e4aa5c89f1096eeb436cb05
harveynw <harveywilliams@live.com>
2023-11-22 11:53:59 +0000

    Add Transform class

---
+++ include/tinyrender/Transform.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <algorithm>
+#include <glm/glm.hpp>
+
+#include "Fwd.hpp"
+
+
+namespace tinyrender {
+    class Transform {
+    protected:
+        glm::mat4 t;
+    public:
+        Transform() {
+            t = glm::mat4(1.0f);
+        }
+        Transform(glm::mat4 _t) {
+            t = _t;
+        }
+        ~Transform() = default;  
+
+        Transform(const Transform &transform): t(transform.t); // copy constructor
+        Transform(Transform &&other) noexcept: t(std::exchange(other.t, glm::mat4(1.0f))) {}; // move constructor
+        Transform& operator=(const Transform &other) { // copy assignment
+            return *this = Transform(other);
+        };
+        Transform& operator=(Transform &&other) noexcept { // move assignment
+            std::swap(t, other.t);
+            return *this;
+        };
+    };
+};
