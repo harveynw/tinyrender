cc3444f2c2d1b42dd174eeca04036b5fd7b27ece
harveynw <harveywilliams@live.com>
2023-12-08 16:01:18 +0000

    Cleaned up examples

--- examples/boat/main.cpp
+++ examples/boat/main.cpp
@@ -5,12 +5,9 @@
 
 #include "tinyrender/Engine.hpp"
 #include "tinyrender/camera/TurntableCamera.hpp"
-#include "tinyrender/camera/FreeviewCamera.hpp"
 #include "tinyrender/objects/Mesh.hpp"
 #include "tinyrender/objects/Cube.hpp"
-#include "tinyrender/objects/Geometry.hpp"
 #include "tinyrender/objects/WaveSim.hpp"
-#include "tinyrender/objects/Voxels.hpp"
 
 using glm::vec3;
 
@@ -26,20 +23,16 @@ int main (int, char**) {
      * Set helper class for moving camera about
      */
 
-    //auto camera = std::make_shared<TurntableCamera>(uniforms);
-    auto camera = std::make_shared<FreeviewCamera>();
+    auto camera = std::make_shared<TurntableCamera>();
+    //auto camera = std::make_shared<FreeviewCamera>();
     engine->setCamera(camera);
 
     /*
      * Create the objects
      */
-    glm::vec3 t = glm::vec3(60.0, -35.0, 35.0);
-    auto boat_t = t + glm::vec3(0, 0, 0.5);
     {
         std::shared_ptr<tinyrender::Object> object = std::make_shared<tinyrender::Mesh>("resources/fourareen.obj");
         engine->addObject(object);
-
-        object->setTranslation(boat_t);
         object->setTexture("resources/fourareen2K_albedo.jpg");
     }
     {
@@ -52,29 +45,13 @@ int main (int, char**) {
         engine->objects.push_back(object);
     }
     {
-        auto object = std::make_shared<tinyrender::Pyramid>(vec3(0, 0, 3), 2, 3);
-        engine->addObject(object);
-
-        object->setScale(0.2f);
-        object->setTranslation(vec3(0.0, 0.0, 5.0));
-        object->setColor(vec3(1.0, 0.0, 1.0));
-        engine->objects.push_back(object);
-    }
-    {
         // Wave Sim
         auto object = std::make_shared<tinyrender::WaveSim>(50, 50);
         engine->addObject(object);
 
         object->HIDDEN = false;
         object->setColor(vec3(0.0, 0.019, 0.301));
-
-        object->setTranslation(t);
-        object->setScale(1.2f);
-    }
-    {
-        // Voxels
-        auto voxels = std::make_shared<tinyrender::Voxels>();
-        engine->addObject(voxels);
+        object->setTranslation(vec3(0.0, 0.0, -0.5));
     }
 
     /*
